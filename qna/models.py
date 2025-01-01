59cdfcbee02f72f90742f1cc03bc388f3b4ae6cc
Marcin Nowakowski <game.of.no@gmail.com>
2025-01-01 11:28:41 +0100

    QNAMIT-3 1) New entities: survey, patient, 2) Migrations regenerates.

--- qna/models.py
+++ qna/models.py
@@ -1,37 +1,46 @@
 from django.db import models
 from django.urls import reverse
-  
-class Question(models.Model):
+
+class Survey(models.Model):
   title = models.CharField(max_length=255)
   pub_date = models.DateTimeField('Date submitted',auto_now_add=True)
   slug = models.SlugField(unique=True)
-  question = models.TextField()
+  description = models.TextField()
   public = models.BooleanField(default=False)
-  allow_comments = models.BooleanField(default=True)
 
   def __str__(self):
     return self.title
-    
-  def get_absolute_url(self):
-    return reverse('question_detail', kwargs={'slug': self.slug})
-    
-  def save(self, *a, **kw):
-    if not self.slug:
-      from django.template.defaultfilters import slugify
-      self.slug = slugify(self.title)[:50]
-    super(Question, self).save(*a, **kw)
 
-class SurveySubmission(models.Model):
-  pesel = models.CharField(max_length=11)
+class Question(models.Model):
+  title = models.CharField(max_length=255)
+  survey = models.ForeignKey(Survey, on_delete=models.DO_NOTHING)
   pub_date = models.DateTimeField('Date submitted',auto_now_add=True)
+  slug = models.SlugField(unique=True)
+  question_text = models.TextField()
+  public = models.BooleanField(default=False)
 
   def __str__(self):
-    return self.pesel + ":" + str(self.pub_date)
+    return str(self.survey) + ":" + self.title
+
+class Patient(models.Model):
+  patient_number = models.CharField(max_length=50)
+  registered_at = models.DateTimeField('Date submitted',auto_now_add=True)
+
+  def __str__(self):
+    return self.patient_number
+
+class SurveySubmission(models.Model):
+  patient = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name='survey_submissions')
+  survey = models.ForeignKey(Survey, on_delete=models.DO_NOTHING)
+  submitted_at = models.DateTimeField('Date submitted',auto_now_add=True)
+
+  def __str__(self):
+    return str(self.submitted_at) + ":" + str(self.patient) + ":" + str(self.survey)
 
 class Answer(models.Model):
   survey_submission = models.ForeignKey(SurveySubmission, on_delete=models.CASCADE, related_name='answers')
   question = models.ForeignKey(Question, on_delete=models.DO_NOTHING)
-  answer = models.TextField()
+  answer_text = models.TextField()
 
   def __str__(self):
     return str(self.survey_submission) + ":" + str(self.question)
