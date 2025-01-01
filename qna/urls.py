3d0a26b3f2194f36e1f7593591e7c78c53e2df43
Marcin Nowakowski <game.of.no@gmail.com>
2025-01-01 19:29:04 +0100

    QNAMIT-3 Templates generated for: 1) register patient, 2) select survey.

--- qna/urls.py
+++ qna/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.QuestionListView.as_view(), name='question_list'),
-    path('create/', views.QuestionCreateView.as_view(), name='question_create'),  # Ensure this is present
-    path('<slug:slug>/', views.QuestionDetailView.as_view(), name='question_detail'),
-    path('<slug:slug>/edit/', views.QuestionEditView.as_view(), name='question_edit'),
+    path('', views.PatientRegisterView.as_view(), name='patient_register'),
+    path('patient_register/', views.PatientRegisterView.as_view(), name='patient_register'),
+    path('survey_select/<str:patient_number>/', views.SurveySelectView.as_view(), name='survey_select'),
+    path('survey_submission_create/<str:patient_number>/<slug:survey_selected>', views.SurveySubmissionCreateView.as_view(), name='survey_submission_create'),
 ]
